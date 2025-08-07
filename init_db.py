3573cee69098ecd369b51fce6611d29caa86b569
Endry Mateo <70662937+EndryMateo@users.noreply.github.com>
2025-08-07 17:41:49 -0400

    fixing bugs

--- init_db.py
+++ init_db.py
@@ -1,16 +1,19 @@
 from database import Base, engine
-from models import Project
+from models import Project, Admin, PasswordResetCode  # ✅ Importar todos los modelos
 from sqlalchemy import inspect
 
 print("Conectando a la base de datos...")
 print(engine)
 
+# Inspecciona antes
 inspector = inspect(engine)
 print("Antes de crear:")
 print(inspector.get_table_names())
 
+# Crea todas las tablas
 Base.metadata.create_all(bind=engine)
 
+# Inspecciona después
+inspector = inspect(engine)  # Refresca el inspector
 print("Después de crear:")
-inspector = inspect(engine)  # Reinstancia para refrescar
 print(inspector.get_table_names())
